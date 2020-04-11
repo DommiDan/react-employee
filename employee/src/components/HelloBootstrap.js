529b406c311bc43c4892ee584f75af5919081f87
Leticia Guerra <g.leticia@rocketmail.com>
2020-04-11 17:12:07 -0400

    updating

--- employee/src/components/HelloBootstrap.js
+++ employee/src/components/HelloBootstrap.js
@@ -1,4 +1,10 @@
 import React from "react";
+import logo from "./img1/images/cloud.jpg";
+import logo1 from "./img1/images/aerith.jpg";
+import logo2 from "./img1/images/barret.jpg";
+import logo3 from "./img1/images/seph.png";
+import logo4 from "./img1/images/tifa.jpg";
+// import friends from "./friends.json";
 
 function HelloBootstrap() {
   return (
@@ -31,11 +37,10 @@ function HelloBootstrap() {
           </tr>
         </thead>
         <thead>
-          
           <tr>
           <th scope="row">1</th>
           <td>
-            <img src="images/18_Event_Cloud.0.jpg" 
+            <img src={logo} 
             width="150" 
             height="150">
             </img>
@@ -44,6 +49,66 @@ function HelloBootstrap() {
             <td>123-456-7890</td>
             <td>cloud@example.com</td>
             <td>08-11-90</td>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+          <th scope="row">2</th>
+          <td>
+            <img src={logo1}  
+            width="190" 
+            height="150">
+            </img>
+          </td>
+          <td>Aerith</td>
+            <td>098-765-4321</td>
+            <td>arieth@example.com</td>
+            <td>02-07-89</td>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+          <th scope="row">3</th>
+          <td>
+            <img src={logo2}  
+            width="150" 
+            height="150">
+            </img>
+          </td>
+          <td>Barret</td>
+            <td>246-809-7531</td>
+            <td>b.arret@example.com</td>
+            <td>10-30-64</td>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+          <th scope="row">4</th>
+          <td>
+            <img src={logo3}  
+            width="150" 
+            height="150">
+            </img>
+          </td>
+          <td>Sephiroth</td>
+            <td>135-790-8642</td>
+            <td>badguy.with.wings.seph@example.com</td>
+            <td>07-11-84</td>
+          </tr>
+        </thead>
+        <thead>
+          <tr>
+          <th scope="row">5</th>
+          <td>
+            <img src={logo4} 
+            width="150" 
+            height="150">
+            </img>
+          </td>
+          <td>Tifa</td>
+            <td>112-223-4456</td>
+            <td>Tifa.is.awesome@example.com</td>
+            <td>05-03-90</td>
           </tr>
         </thead>
    </table>
